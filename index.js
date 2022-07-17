0d979e0157c0b186dffd85585076b8334c740f42
Mimi Majgaard <mimimajgaard@gmail.com>
2022-07-17 15:02:01 -0700

    commit tree f41d63922b79ca919aead550a133eb67c3984848

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+function submitData(name, email){
+    const formData = {
+        name: name,
+        email: email
+    };
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            // Get the _id_ from object and append to the DOM
+            const id = object['id'];
+            // ..and append to the DOM
+            // document.querySelector(whatelement).whateverYouNeedToDo
+            document.querySelector('#output').innerHTML = id;
+        })
+        .catch(function (error) {
+            document.querySelector('#output').innerHTML = error.message  
+        })
+
+        
+}
+
